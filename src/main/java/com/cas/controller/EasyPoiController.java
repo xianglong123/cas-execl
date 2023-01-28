ba404ad3ba47292da034610a8b14c883dad219ed
xianglong123 <xianglong1233@163.com>
2023-01-28 15:21:45 +0800

    yh

--- src/main/java/com/cas/controller/EasyPoiController.java
+++ src/main/java/com/cas/controller/EasyPoiController.java
@@ -1,9 +1,17 @@
 package com.cas.controller;
 
+import com.cas.utils.ExcelUtils;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author xiang_long
@@ -19,12 +27,23 @@ public class EasyPoiController {
     /**
      * 获取案例模版
      */
-    @GetMapping("/model")
-    public void model() {
-
-
-
+    @GetMapping("/model/{fileName}")
+    public void model(@PathVariable("fileName") String fileName) {
+        System.out.println("下载文件名 " + fileName);
     }
+
+    /**
+     * 导入数据
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @PostMapping(value = "/import")
+    public void importExcel(@RequestParam("file") MultipartFile file) throws IOException {
+    }
+
+
+
 
 
 
