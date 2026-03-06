1a579f18bc77ba6625fe2ec1526234a6052531c4
xianglong <1391086179@qq.com>
2026-03-06 13:06:30 +0800

    测试CI/CD

--- src/main/java/com/cas/controller/ZipController.java
+++ src/main/java/com/cas/controller/ZipController.java
@@ -48,7 +48,7 @@ public class ZipController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "files", value = "压缩多个文件", paramType = "formData", allowMultiple = true, required = true, dataType = "file")
     })
-    @PostMapping("compressionMore5")
+    @PostMapping("compressionMore6")
     public void compressionMore(HttpServletRequest request,
                                HttpServletResponse response,
                                @ApiParam(value = "上传文件", required = true) @RequestParam(value = "files") List<MultipartFile> multipartFiles
